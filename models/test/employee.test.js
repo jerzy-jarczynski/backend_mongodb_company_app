0de9f9a89bfcdd4c6d756298bc640e7b418ac602
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-26 19:37:47 +0200

    Add tests for Employee model

---
+++ models/test/employee.test.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Employee = require('../employee.model.js');
+const expect = require('chai').expect;
+
+describe('Employee', () => {
+
+  it('should throw an error if no args', () => {
+    const emp = new Employee({}); // create new Employee, but don't set any attr values
+  
+    emp.validate(err => {
+      expect(err.errors.firstName).to.exist;
+      expect(err.errors.lastName).to.exist;
+      expect(err.errors.department).to.exist;
+    });
+  });
+
+  it('should throw an error if args are not strings', () => {
+
+    const cases = [{}, []];
+    for(let name of cases) {
+      const emp = new Employee({ firstName: name, lastName: name, department: name });
+  
+      emp.validate(err => {
+        expect(err.errors.firstName).to.exist;
+        expect(err.errors.lastName).to.exist;
+        expect(err.errors.department).to.exist;
+      });
+  
+    }
+  });
+
+  it('should not throw an error if args are okay', () => {
+
+    const emp = new Employee({ firstName: 'John', lastName: 'Doe', department: 'Management' });
+  
+    emp.validate(err => {
+      expect(err).to.not.exist;
+    });
+  
+  });
+
+  after(() => {
+    mongoose.models = {};
+  });
+
+});
